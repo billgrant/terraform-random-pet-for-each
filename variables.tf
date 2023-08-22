c3d9d5cddcdd3af5caa4a0d5b2d99e87e2668333
billgrant <bill.grant@hashicorp.com>
2023-08-22 11:57:20 -0400

    added validation rule

--- variables.tf
+++ variables.tf
@@ -4,4 +4,11 @@ variable "pets" {
     prefix     = string
     separator = string
   }))
+  validation {
+    # Validate the length is greater than 0
+    condition = alltrue([
+      for v in var.pets : !(v.separator == "_") 
+    ])
+    error_message = "Underscore is not an allowed separator"
+  }
 }
